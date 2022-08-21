d2f6b8461a24bb0ff0fe4b19123ffa745627888a
AndriyKozh <max190716@ukr.net>
2022-08-21 13:58:55 +0300

    q1

--- src/js/api-servis.js
+++
@@ -1,9 +0,0 @@
-const BASE_URL = 'https://pokeapi.co/api/v2';
-
-function fetchPokemonId(pokemonId) {
-  return fetch(`${BASE_URL}/pokemon/${pokemonId}`).then(response => {
-    return response.json();
-  });
-}
-
-export default { fetchPokemonId };
--- src/js/refs.js
+++
@@ -1,6 +0,0 @@
-export default function getRefs() {
-  return {
-    cardConteiner: document.querySelector('.js-card-container'),
-    searchForm: document.querySelector('.js-search-form'),
-  };
-}
